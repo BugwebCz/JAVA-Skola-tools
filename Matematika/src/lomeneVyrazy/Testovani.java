2704d71abed8af4e378cd5f0472220d38df3e86c
emouralova <emouralova@gymtrebon.cz>
2020-04-14 10:38:09 +0200

    upgrade

--- Matematika/src/lomeneVyrazy/Testovani.java
+++ Matematika/src/lomeneVyrazy/Testovani.java
@@ -9,22 +9,29 @@ import javax.script.ScriptException;
 public class Testovani {
 
 	public static void main(String[] args) throws ScriptException {
-		
+			
 		Scanner sken = new Scanner(System.in, "UTF-8");
 		String Vyraz = sken.nextLine();
-		String priklad = Vyraz.substring(Vyraz.indexOf("=", 0) + 1, Vyraz.length() + 1);
+		String priklad = "var " + Vyraz;
 		
-		ScriptEngineManager mgr = new ScriptEngineManager();
-		ScriptEngine engine = mgr.getEngineByName("JavaScript");
-		    
-		Object y11 = engine.eval(priklad.replaceAll("x", "10"));
-		float y1 = (float) y11;
-		    
-		Object y22 = engine.eval(priklad.replaceAll("x", "-10"));
-		float y2 = (float) y22;
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+		 
+		float x1 = 10;
+		float x2 = -10;
+		
+		manager.put("x", 10);
+		engine.eval(priklad);
+		float y1 = (float) engine.get("y");
+		
+		manager.put("x", -10);
+		engine.eval(priklad);
+		float y2 = (float) engine.get("y");
 		
 		System.out.println(y1 + y2);
 		
+	 
+
 		
 	}
 	
